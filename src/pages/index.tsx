80053f950d751b5cfa9f19dd0d7750ddca409136
albrimpaqarzi <albrim.paqarizi@ornio.no>
2022-01-16 10:25:55 +0100

    Added Emotion styled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,19 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
+// emotion
+import styled from '@emotion/styled';
+
 // mui
 import { Container, Box, Typography, Link } from '@mui/material';
+
+const BoxStyled = styled(Box)({
+  color: 'hotpink',
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
 
 const Home: NextPage = () => (
   <Container maxWidth="md">
@@ -11,17 +22,14 @@ const Home: NextPage = () => (
       <title>Create Next App</title>
     </Head>
 
-    <Box
-      component="main"
-      sx={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-    >
+    <BoxStyled component="main">
       <Typography variant="h4" align="center">
         Welcome to{' '}
         <Link underline="hover" href="https://nextjs.org">
           Next.js!
         </Link>
       </Typography>
-    </Box>
+    </BoxStyled>
   </Container>
 );
 
